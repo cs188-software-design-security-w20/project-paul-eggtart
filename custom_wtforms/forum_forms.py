396afdaf7cd42f0884a5b1a07dfb4ba0946b81bf
Sean Yin <sean999_y@yahoo.com>
2020-01-12 10:30:34 -0800

    created rating form, headings may require a little work, set up it's database table, maybe rename TA table to comments table in the future

--- custom_wtforms/forum_forms.py
+++ custom_wtforms/forum_forms.py
@@ -2,11 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 from wtforms import TextAreaField,TextField
-
 from wtforms.fields.html5 import IntegerField
 
-class forum(FlaskForm):
+class comment_form(FlaskForm):
     comment = StringField('comment', validators=[DataRequired()])
 
-class slider(FlaskForm):
-    clarity = IntegerField('clarity', default=0)
+class rating_form(FlaskForm):
+    clarity = IntegerField('clarity', default=3)
+    helpfulness = IntegerField('helpfulness', default=3)
+    availability = IntegerField('availability', default=3)
