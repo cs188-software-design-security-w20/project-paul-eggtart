4711e9de4e03ac3ef0e59ac330408b9302edbaf3
Ryan Yang <Ryanyang99@hotmail.com>
2020-01-14 15:09:30 -0800

    update

--- search.py
+++ search.py
@@ -3,5 +3,4 @@ from wtforms.validators import DataRequired
 from wtforms import TextAreaField,StringField,validators
 
 class searchBar(FlaskForm):
-    #ta_name = StringField('ta_name', validators=[DataRequired()])
     ta_name = StringField('ta_name', [validators.Length(min=1, max=50)])
