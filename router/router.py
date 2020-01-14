749c82f13ac6383c73a03bab3baf19a99094b0e7
Sean Yin <sean999_y@yahoo.com>
2020-01-14 08:44:40 -0800

    temp while upgrading python, added datetime / rating stuff

--- router/router.py
+++ router/router.py
@@ -8,10 +8,10 @@ from flask import (
 from custom_wtforms.forum_forms import comment_form, rating_form
 from search import searchBar
 from load import database
-from forms import forum
 import datetime
 
-db = database() #define database
+# define the database
+db = database()
 
 
 router = Blueprint(
@@ -23,24 +23,40 @@ router = Blueprint(
 
 @router.route('/TA/<ta_name>', methods=['GET', 'POST'])
 def TA(ta_name):
+    ta_object = db.child("TA").child(ta_name).get()
     ta_info = []
-    ta = db.child("TA").child(ta_name).get()
-    data = []
-    for _, val in ta.val().items():
-        if val.get("comment")!= None: #comment
-            data.append( (val["comment"],val["timestamp"].split(" ")[0]))
+    comments = []
+    comment_datetime = []
+    ratings = [0, [0, 0, 0]]
 
-        elif val.get("rating")!= None: # rating
-            print(val["rating"])
+    for _, val in ta_object.val().items():
+        if val.get("comment") != None and val.get("comment_datetime"):
+            str_datetime = val["comment_datetime"]
+            str_datetime = datetime.fromisoformat(str_datetime).strftime("%m/%d/%Y, %H:%M:%S")
+            comments.append(val["comment"], str_datetime)
+        if val.get("rating") != None:
+            ratings[0] += 1
+            ratings[1][0] += val["rating"]["clarity"]
+            ratings[1][1] += val["rating"]["helpfulness"]
+            ratings[1][2] += val["rating"]["availability"]
+    
+    ratings[1][0] /= ratings[0]
+    ratings[1][1] /= ratings[0]
+    ratings[1][2] /= ratings[0]
 
-    ta_info= (ta_name,data) 
+    ta_info.append((ta_name, comments, ratings[1]))
+    print(ta_info)
 
     # adding comment to forum
     my_comment = forum()
     if my_comment.validate_on_submit():
-
-        db.child("TA").child(ta_name).push({"comment": my_comment.comment.data,
-                                        "timestamp": str(datetime.datetime.now()) })
+        comment_datetime = datetime.datetime.now().isoformat()
+        # print(my_comment.comment.data)
+        # print(comment_datetime)
+        db.child("TA").child(ta_name).push({
+            "comment": my_comment.comment.data,
+            "comment_datetime": comment_datetime
+        })
         return redirect('/TA/'+ta_name)
     else:
         print("error, too long")
@@ -48,10 +64,15 @@ def TA(ta_name):
     # addint rating to TA
     my_rating = rating_form()
     if my_rating.validate_on_submit():
-        print(my_rating.clarity.data)
-        print(my_rating.helpfulness.data)
-        print(my_rating.availability.data)
-        db.child("Ratings").child(ta_name).push({"rating": [my_rating.clarity.data, my_rating.helpfulness.data, my_rating.availability.data]})
+        print([my_rating.clarity.data, my_rating.helpfulness.data, my_rating.availability.data])
+        db.child("TA").child(ta_name).push({
+            "rating": {
+                "clarity": my_rating.clarity.data, 
+                "helpfulness": my_rating.helpfulness.data, 
+                "availability":my_rating.availability.data
+            }
+        })
+        print("pushed")
 
     return render_template('ta_page.html', ta_info=ta_info, redirect='/TA/'+ta_name, 
         comment_form=my_comment, rating_form=my_rating, ta_jpg= ta_name+".jpg")
