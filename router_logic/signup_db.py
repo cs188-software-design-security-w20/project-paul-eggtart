c57c3a06144342311bb982f7cab536a8066b0a93
Jeffrey Zhu <jeffrey7221@gmail.com>
2020-02-05 14:27:00 -0800

    merge with Ryan

--- router_logic/signup_db.py
+++ router_logic/signup_db.py
@@ -12,27 +12,33 @@ from wtforms import TextAreaField, StringField, validators
 from load import database
 from profile.profile import User
 
-class signUp(FlaskForm):
+class SignUpForm(FlaskForm):
     first_name = StringField('first_name', [validators.Length(min=1, max=50)])
     last_name = StringField('last_name', [validators.Length(min=1, max=50)])
     email_addr = StringField('email_addr', [validators.Length(min=1, max=50)])
     password = StringField('password', [validators.Length(min=1, max=50)])
 
-def create_user(db, signup_form):
-    print(signup_form.first_name.data)
-    print(signup_form.last_name.data)
-    new_user = User()
-    new_user.id = 0
-    new_user.email = signup_form.email_addr.data
-    new_user.first_name = signup_form.first_name.data
-    new_user.last_name = signup_form.last_name.data
-    new_user.password = signup_form.password.data
-    new_user.authenticated = True
-    new_user.password_reset = None
-    new_user.credits = 0
-    new_user.viewable_ta = []
-    new_user.remaining_views = 3
-    db.child("users").push(jsonify(new_user))
+    def create_user(self, db, signup_form):
+        print(signup_form.first_name.data)
+        print(signup_form.last_name.data)
+        new_user = User()
+        new_user.id = 0
+        new_user.email = signup_form.email_addr.data
+        new_user.first_name = signup_form.first_name.data
+        new_user.last_name = signup_form.last_name.data
+        new_user.password = signup_form.password.data
+        new_user.authenticated = True
+        new_user.password_reset = None
+        new_user.credits = 0
+        new_user.viewable_ta = []
+        new_user.remaining_views = 3
+        json_user = new_user.toJSON()
+        json_user.replace('"', "'")
+        dummy = {"name": "Lana Kane", "agency": "Figgis Agency"}
+        print(dummy)
+        print(json_user.toString())
+        db.child("test_users").child(new_user.id).set(json_user)
+        #  push(new_user.toJSON())
 
 
 # class User:
