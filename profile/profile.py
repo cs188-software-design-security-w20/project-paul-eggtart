ef0c66eb13c9a6140e779a0231338848eb5e212f
Jeffrey Zhu <jeffrey7221@gmail.com>
2020-02-06 10:58:45 -0800

    no longer init with id

--- profile/profile.py
+++ profile/profile.py
@@ -50,8 +50,8 @@ class User(UserMixin):
     def is_anonymous(self):
         return False
 
-    def __init__(self, id):
-        self.id = id
+    def __init__(self):
+        return
     
     def get_id(self):
         try:
