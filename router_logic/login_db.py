df694ae9e4c796ea8e8caebf68557b0b55189757
Ryan Yang <Ryanyang99@hotmail.com>
2020-02-20 14:56:30 -0800

    updated security for XSS

--- router_logic/login_db.py
+++ router_logic/login_db.py
@@ -28,11 +28,11 @@ class LoginForm(Form):
         for u in users.each():
             data = u.val()
             if data['email'] == user.email and decrypter.decrypt(data["password"],user.password): #data["password"] == user.password:
-                last_lockout = data['last_lockout']
-                last_lockout = datetime.datetime.strptime(last_lockout, '%Y-%m-%dT%H:%M:%S.%f')
-                if last_lockout > datetime.datetime.now():
-                    flash("Too many login attempts. Please try again in 5 minutes")
-                    return -4
+                # last_lockout = data['last_lockout']
+                # last_lockout = datetime.datetime.strptime(last_lockout, '%Y-%m-%dT%H:%M:%S.%f')
+                # if last_lockout > datetime.datetime.now():
+                #     flash("Too many login attempts. Please try again in 5 minutes")
+                #     return -4
                 if data['authenticated'] is False:
                     flash("Account not authenticated. Please reauthenticate.")
                     return -2
@@ -42,26 +42,26 @@ class LoginForm(Form):
                     flash("Password has expired. Please reset your password")
                     return -3
                 id = data['id']
-                db.child('users').child(id).update({"num_lockouts": 0})
+                #db.child('users').child(id).update({"num_lockouts": 0})
                 return int(data['id'])
             #incorrect password
-            elif data['email'] == user.email:
-                id = data['id']
-                num_lockouts = data['num_lockouts']
-                last_lockout = data['last_lockout']
-                last_lockout = datetime.datetime.strptime(last_lockout, '%Y-%m-%dT%H:%M:%S.%f')
-                num_lockouts += 1
-                if last_lockout < datetime.datetime.now():
-                    if num_lockouts % 5 == 0:
-                        last_lockout = datetime.datetime.now() + timedelta(minutes=5)
-                        last_lockout = last_lockout.isoformat()
-                        flash("Too many login attempts. Please try again in 5 minutes")
-                        db.child('users').child(id).update({"num_lockouts": num_lockouts, "last_lockout": last_lockout})
-                        return -4
-                    else:
-                        db.child('users').child(id).update({"num_lockouts": num_lockouts})
-                        return -1
-                else:
-                    flash("Too many login attempts. Please try again in 5 minutes")
-                    return -2
+            # elif data['email'] == user.email:
+            #     id = data['id']
+            #     num_lockouts = data['num_lockouts']
+            #     last_lockout = data['last_lockout']
+            #     last_lockout = datetime.datetime.strptime(last_lockout, '%Y-%m-%dT%H:%M:%S.%f')
+            #     num_lockouts += 1
+            #     if last_lockout < datetime.datetime.now():
+            #         if num_lockouts % 5 == 0:
+            #             last_lockout = datetime.datetime.now() + timedelta(minutes=5)
+            #             last_lockout = last_lockout.isoformat()
+            #             flash("Too many login attempts. Please try again in 5 minutes")
+            #             db.child('users').child(id).update({"num_lockouts": num_lockouts, "last_lockout": last_lockout})
+            #             return -4
+            #         else:
+            #             db.child('users').child(id).update({"num_lockouts": num_lockouts})
+            #             return -1
+            #     else:
+            #         flash("Too many login attempts. Please try again in 5 minutes")
+            #         return -2
         return -1
