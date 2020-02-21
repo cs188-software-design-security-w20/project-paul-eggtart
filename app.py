1a0060890082183c0096d01b8d331c3704525baa
Ryan Yang <Ryanyang99@hotmail.com>
2020-02-20 16:24:08 -0800

    updated csp

--- app.py
+++ app.py
@@ -22,22 +22,32 @@ import load
 login_manager = LoginManager()
 
 
+
 csp = {
- 'default-src': [
+    'default-src': [
         '\'self\'',
+        '\'unsafe-inline\'',
+        'stackpath.bootstrapcdn.com',
+        'code.jquery.com',
+        'cdn.jsdelivr.net',
         'https://rate-my-ta.herokuapp.com/',
         'https://cdnjs.cloudflare.com',
         'https://fonts.googleapis.com/icon?family=Material+Icons',
         'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
         'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',
         'http://farm8.staticflickr.com/7064/6858179818_5d652f531c_h.jpg',
-        'https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Open+Sans:400,300,600'
+        'https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Open+Sans:400,300,600',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+        '*'
+    ],
+    'img-src': '*',
+    'script-src':'*',
+    'style-src':'*',
+    'font-src':'*'
 
 
-    ]
 }
-
-
 
 
 def create_app(config_file):
